0c7ee2cc4b990184177d05b7827d2f33aeae44bb
min368655-web <min368655@gamil.com>
2025-10-21 17:08:18 +0900

    10.21 커밋할게요

--- lee/js/filter.js
+++ lee/js/filter.js
@@ -1,4 +1,4 @@
-// filter.js
+/* // filter.js
 const buttons = document.querySelectorAll('.btn');
 const shows = document.querySelectorAll('.show-item');
 
@@ -20,4 +20,127 @@ buttons.forEach(button => {
       }
     });
   });
+}); */
+/* const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+let index = 0;
+
+function showSlide(n) {
+  slides[index].classList.remove("active");
+  dots[index].classList.remove("active");
+  index = (n + slides.length) % slides.length;
+  slides[index].classList.add("active");
+  dots[index].classList.add("active");
+}
+
+// auto-play every 6 sec
+let slideInterval = setInterval(() => showSlide(index + 1), 6000);
+
+// nav buttons
+document
+  .querySelector(".prev")
+  .addEventListener("click", () => showSlide(index - 1));
+document
+  .querySelector(".next")
+  .addEventListener("click", () => showSlide(index + 1));
+
+// dots click
+dots.forEach((dot) => {
+  dot.addEventListener("click", () => showSlide(parseInt(dot.dataset.index)));
 });
+
+
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+ScrollSmoother.create({
+  smooth: 1,
+  effects: true,
+  normalizeScroll: true
+});
+ */
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+ScrollSmoother.create({
+  smooth: 1,
+  effects: true,
+  normalizeScroll: true
+});
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".zoom-container",
+      start: "top top",
+      end: "+=150%",
+      pin: true,
+      // markers: true,
+      scrub: 1
+    }
+  })
+  .to(
+    ".zoom-item[data-layer='3']",
+    {
+      opacity: 1,
+      z: 800,
+      ease: "power1.inOut"
+    },
+    0
+  )
+  .to(
+    ".zoom-item[data-layer='2']",
+    {
+      opacity: 1,
+      z: 600,
+      ease: "power1.inOut"
+    },
+    0
+  )
+  .to(
+    ".zoom-item[data-layer='1']",
+    {
+      opacity: 1,
+      z: 400,
+      ease: "power1.inOut"
+    },
+    0
+  )
+  .to(
+    ".heading",
+    {
+      opacity: 1,
+      z: 50,
+      ease: "power1.inOut"
+    },
+    0
+  );
+
+const splitLetters = SplitText.create(
+  document.querySelector(".opacity-reveal")
+);
+gsap.set(splitLetters.chars, { opacity: "0.2", y: 0 });
+
+gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".section-stick",
+      pin: true,
+      start: "center center",
+      end: "+=1500",
+      //markers: true,
+      scrub: 1
+    }
+  })
+  .to(splitLetters.chars, {
+    opacity: "1",
+    duration: 1,
+    ease: "none",
+    stagger: 1
+  })
+  .to({}, { duration: 10 })
+  .to(".opacity-reveal", {
+    opacity: "0",
+    scale: 1.2,
+    duration: 50
+  });
